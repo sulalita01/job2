85013fc2a887baa6cdd5bd5871386fb2111e1968
sulalita kunin <sulalitakunin@gmail.com>
2024-06-05 14:44:46 +0700

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    // { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    // { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    // { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'หน้าแรก', url: '/home', icon: 'home' },
+    { title: 'ประวัติส่วนตัว', url: '/about', icon: 'person' },
+  ];
+  public labels = '';
+  constructor() {}
+}
